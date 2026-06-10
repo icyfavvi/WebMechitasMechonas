27b905f1bdad04540b23f2174129ff5df5490f68
icyfavvi <kiddonachou@gmail.com>
2026-06-10 15:33:25 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import ProductCatalog from "./components/ProductCatalog"
